827ab5bed7dcef032588df094f3138d6b5efd065
coolguy <notarealaddress@gmail.com>
2022-03-05 00:02:24 -0600

    add datail page for each movie, deploy Youtube API

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,30 +1,51 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG = "MovieAdapter"
+
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() { // write this after finishing the declaration of ViewHolder (next line)
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
     // ViewHolder is a component already defined by RecycleView
     // the ViewHolder we defined here is to extend the above component
         private val tvPoster = itemView.findViewById<ImageView>(R.id.tvPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init{
+            itemView.setOnClickListener((this))
+        }
+
         fun bind(movie: Movie){
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             // special treatment for image loading
             Glide.with(context).load(movie.posterImageUrl).into(tvPoster)
 
+        }
+
+        override fun onClick(p0: View?) {
+            // 1. Get notified of the particular movie which was clicked
+            val movie = movies[adapterPosition]
+            // validate the above line work, a visual indicator of what happen:
+            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+            // 2. Use the intent system to navigate to the new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            // should do sth about Parcelize
+            intent.putExtra("MOVIE_EXTRA", movie)
+            context.startActivity(intent)
         }
     }
     // expensive operation: create a view
